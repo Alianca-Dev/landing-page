4236d3b78538d3f05cd80ad97ae685dd0ab03f19
Leonardo de Andrade <leonardo.andrade@actionlabs.com.br>
2020-05-17 18:07:36 -0300

    Added homolog credentials

---
+++ src/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    FIREBASE_API_KEY: "AIzaSyC8hOB3HQxwC-UwTm75njJFre_ee2fU1OQ",
+    FIREBASE_AUTH_DOMAIN: "alianca-dev-homolog.firebaseapp.co",
+    FIREBASE_DATABASE_URL: "https://alianca-dev-homolog.firebaseio.com",
+    FIREBASE_PROJECT_ID: "alianca-dev-homolog",
+    FIREBASE_STORAGE_BUCKET: "alianca-dev-homolog.appspot.com",
+    FIREBASE_MESSAGING_SENDER_ID: "172123573535",
+    FIREBASE_APP_ID: "1:172123573535:web:c5f19f55b9fcb525edb42a",
+    FIREBASE_MEASUREMENT_ID: "G-LYJBDDK72E"
+}
